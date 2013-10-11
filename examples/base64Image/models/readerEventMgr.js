8a313626964e5b51e76aa4470cc23e10d7b41760
SeungHyun PAEK <pekuid@gmail.com>
2013-10-11 16:39:03 +0900

    script error 수정

--- examples/base64Image/models/readerEventMgr.js
+++ examples/base64Image/models/readerEventMgr.js
@@ -6,13 +6,5 @@ tipJS.model("readerEventMgr", {
 		var imageReader = globalModel.imageReader;
 
 		imageReader.onload = tipJS.action.onLoad;
-
-		imageReader.onprogress = function(e){
-			var loadData = e.loaded;
-			var totalData = e.total;
-			var per = (loadData/totalData) * 100;
-			globalModel.loadingBar.innerHTML = per+"% ("+loadData+"/"+totalData+" Bytes)";
-			globalModel.loadingBar.value = per;
-		};
 	}
 });
